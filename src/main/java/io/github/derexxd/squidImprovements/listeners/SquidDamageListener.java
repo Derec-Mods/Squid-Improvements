3c9546d61e805b4e576920e38797463c70d8183a
Derek <derex1987@gmail.com>
2026-07-28 03:16:01 -0500

    feat: player gets blinded

--- src/main/java/io/github/derexxd/squidImprovements/listeners/SquidDamageListener.java
+++ src/main/java/io/github/derexxd/squidImprovements/listeners/SquidDamageListener.java
@@ -1,12 +1,41 @@
 package io.github.derexxd.squidImprovements.listeners;
 
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Squid;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class SquidDamageListener implements Listener {
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof Player player)) {
+            return;
+        }
+        if (!(event.getEntity() instanceof Squid squid)) {
+            return;
+        }
+
+        squid.getWorld().spawnParticle(
+                Particle.SQUID_INK,
+                squid.getLocation(),
+                24,
+                0.4,
+                0.4,
+                0.4,
+                0.02
+        );
+
+        player.addPotionEffect(new PotionEffect(
+                PotionEffectType.BLINDNESS,
+                ThreadLocalRandom.current().nextInt(40, 61),
+                0
+        ));
     }
 }
